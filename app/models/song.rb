41db7c65908fef08ee59973f70b7be9fb069bd48
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-07-08 22:21:10 -0600

    done

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,10 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+
+  def artist_name
+    artists_name = self.artist.name
+    #song_title = self.title
+    #"#{artist_name} - #{song_title}"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+artist = Artist.create(name: "Phish")
+artist.songs.create(title: "Tweezer")
+artist.songs.create(title: "Carini")
+artist.songs.create(title: "You Enjoy Myself")
+artist.songs.create(title: "Fluffhead")
+
+artist2 = Artist.create(name: "Widespread Panic")
+artist2.songs.create(title: "Barstoold & Dreamers")
+artist2.songs.create(title: "Bowlegged Women")
+artist2.songs.create(title: "Diner")
+artist2.songs.create(title: "Space Wrangler")
